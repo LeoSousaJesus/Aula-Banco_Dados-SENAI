1e9bf59be99631c385189d37a74dd6ce80ce0ebc
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-14 17:47:13 -0300

    Modelo Lógico BD-cliente

---
+++ Aula 10/BD_cliente.sql
@@ -0,0 +1,20 @@
+/*
+1. Projetar Nome, Sobrenome, Logradouro, UF;
+2. Projetar Idcliente, Nome, ddd, Numero;
+3. Projetar Nome, Sobrenome, Logradouro, UF Em Ordem Alfabetica Por UF;
+4. Projetar Idcliente, Nome, Sobrenome, Logradouro, UF, Em Ordem Alfabetica Por Idcliente;
+5. Projetar quantidade de UF cadastrada;
+6. projetar as cidades cadastradas e suas quantidades;
+7. projetar as cidades cadastradas, suas quantidades em ordem alfabetica;
+8. Projetar a quantidade de UFS cadastradas sem repetiçao;
+9. projetar idcliente, nome, sobrenome, UFs, ddds;
+10. projetar idcliente, nome, sobrenome, UFs, ddds, em ordem crescente por ddd.
+11. Projetar nome, sobrenome, logradouro, ddd, numero de telefone do cliente X, NÃO
+ESQUECER DE MOSTRAR QUE REALMENTE É O IDCLIENTE PROCURADO.
+12. Projetar todos os clientes das UF x e da UF y
+13. Projetar todos enderecos de 3 ufs
+14. projetar nome, sobrenome, tipotelefone, ddd, numero por ordem de nome;
+15. projetar nome, sobrenome, logradouro, cidade, uf, tipotelefone, ddd, numero, por ordem de
+uf;
+
+*/
