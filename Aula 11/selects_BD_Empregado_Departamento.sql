05fd3b44cad19de03a873a31a78d4765221a0f9c
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-21 16:51:12 -0300

    Adicionando Selects BD_Empregado_Departamento

--- Aula 11/selects_BD_Empregado_Departamento.sql
+++ Aula 11/selects_BD_Empregado_Departamento.sql
@@ -7,3 +7,8 @@ SELECT d.endereco, e.nomeEmpregado
 FROM Empregado e
 JOIN Departamento d ON e.fk_funcionario = d.idDepartamento
 WHERE e.idEmpregado = 1;
+
+SELECT sexo, COUNT(*) AS quantidade
+FROM Empregado
+GROUP BY sexo;
+
