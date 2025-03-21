7b8f98e5c6257dbc5f712dc6794ca3df4d5d4b3e
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-21 16:40:31 -0300

    Adicionando Selects BD_Empregado_Departamento

---
+++ Aula 11/selects_BD_Empregado_Departamento.sql
@@ -0,0 +1,9 @@
+SELECT e.nomeEmpregado, e.salario, d.endereco
+FROM Empregado e
+JOIN Departamento d ON e.fk_funcionario = d.idDepartamento
+WHERE e.sexo = 'F';
+
+SELECT d.endereco, e.nomeEmpregado
+FROM Empregado e
+JOIN Departamento d ON e.fk_funcionario = d.idDepartamento
+WHERE e.idEmpregado = 1;
