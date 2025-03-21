521142561db9704cf962e750d0fec5a9394a9198
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-21 16:37:56 -0300

    Adicionando BD_Empregado_Departamento

---
+++ Aula 11/BD_Empregado_Departamento.sql
@@ -0,0 +1,18 @@
+create database empregado_departamento;
+use empregado_departamento;
+
+
+create table departamento (
+    idDepartamento int primary key auto_increment,
+    nomeDepartamento varchar(50),
+    endereco varchar(50)
+);
+
+create table empregado (
+    idEmpregado int primary key auto_increment,
+    nomeEmpregado varchar(50),
+    salario DECIMAL(5, 2),
+    sexo char(1),
+    fk_funcionario int,
+    foreign key (fk_funcionario) references departamento(idDepartamento)
+);
