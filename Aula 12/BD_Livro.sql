712670c422bd8cac3bb81e0fce7e16a009535b9a
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-24 17:50:36 -0300

    Adicionando BD_Livro

---
+++ Aula 12/BD_Livro.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE BD_Livros;
+USE BD_Livros;
+
+CREATE TABLE autor (
+    id_autor INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    sexo CHAR(1)
+);
+
+CREATE TABLE categoria (
+    id_categoria INT PRIMARY KEY,
+    tipocategoria VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE editora (
+    id_editora INT PRIMARY KEY,
+    nomeeditora VARCHAR(50) NOT NULL,
+    telefone VARCHAR(12) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(18) NOT NULL
+);
+
+CREATE TABLE livro (
+    id_livro INT PRIMARY KEY,
+    isbn VARCHAR(17) NOT NULL,
+    titulo VARCHAR(50) NOT NULL,
+    datalancamento DATE,
+    numeropagina INT NOT NULL,
+    valor FLOAT(7,2) NOT NULL,
+    fk_id_autor INT,
+    fk_id_editora INT,
+    fk_id_categoria INT,
+    FOREIGN KEY (fk_id_autor) REFERENCES autor(id_autor),
+    FOREIGN KEY (fk_id_editora) REFERENCES editora(id_editora),
+    FOREIGN KEY (fk_id_categoria) REFERENCES categoria(id_categoria)
+);	
