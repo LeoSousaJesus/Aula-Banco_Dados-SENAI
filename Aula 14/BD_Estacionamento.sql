3da2382f88795ac5ee1bc3d6418e05040a1ebf0c
leandro <leandrosousajesus@gmail.com>
2025-03-28 14:19:15 -0300

    Adicionando BD_Estacionamento

--- Aula 14/BD_Estacionamento.sql
+++ Aula 14/BD_Estacionamento.sql
@@ -4,6 +4,14 @@ CREATE DATABASE BD_Estacionamento_110;
 /* Ativar o BD_Estacionamento */
 USE BD_Estacionamento_110;
 
+/* Criar a tabela Cliente */
+CREATE TABLE Cliente (
+	IdCliente INT PRIMARY KEY,
+	Nome VARCHAR (50) NOT NULL,
+    CPF VARCHAR(14) NOT NULL
+);
+
+/* Criar a tabela Veículo */
 CREATE TABLE Veiculo (
 	IdVeiculo INT PRIMARY KEY,
     Placa VARCHAR(8) NOT NULL,
@@ -13,12 +21,7 @@ CREATE TABLE Veiculo (
     FOREIGN KEY (fk_idCLiente) REFERENCES cliente(idCliente)
 );
 
-CREATE TABLE Cliente (
-	IdCliente INT PRIMARY KEY,
-	Nome VARCHAR (50) NOT NULL,
-    CPF VARCHAR(14) NOT NULL
-);
-
+/* Criar a tabela Telefone */
 CREATE TABLE Telefone (
 	idTelefone INT PRIMARY KEY,
     DDD CHAR(2) NOT NULL,
@@ -27,6 +30,21 @@ CREATE TABLE Telefone (
     FOREIGN KEY (fk_idCLiente) REFERENCES cliente(idCliente)
 );
 
+/* Criar a tabela TipoVaga */
+CREATE TABLE TipoVaga (
+	IdTipoVaga INT PRIMARY KEY,
+    TipoVaga VARCHAR(10)
+);
+
+/* Criar a tabela Vaga */
+CREATE TABLE Vaga (
+	IdVaga INT PRIMARY KEY,
+    StatusVaga BOOLEAN NOT NULL DEFAULT 0,
+    fk_IdTipoVaga INT,
+    FOREIGN KEY (fk_IdTipoVaga) REFERENCES TipoVaga(idTipoVaga)
+);
+
+/* Criar a tabela Ticket */
 CREATE TABLE Ticket (
 	IdTicket INT PRIMARY KEY,
     Hora_entrada TIME,
@@ -35,16 +53,4 @@ CREATE TABLE Ticket (
     FOREIGN KEY (fk_idCLiente) REFERENCES cliente(idCliente),
     fk_IdVaga INT,
     FOREIGN KEY (fk_idVaga) REFERENCES Vaga(idVaga)
-);
-
-CREATE TABLE Vaga (
-	IdVaga INT PRIMARY KEY,
-    StatusVaga BOOLEAN NOT NULL DEFAULT 0,
-    fk_IdTipoVaga INT,
-    FOREIGN KEY (fk_IdTipoVaga) REFERENCES TipoVaga(idTipoVaga)
-);
-
-CREATE TABLE TipoVaga (
-	IdTipoVaga INT PRIMARY KEY,
-    TipoVaga VARCHAR(10)
 );
