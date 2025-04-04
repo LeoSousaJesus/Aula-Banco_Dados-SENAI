57eaa6951ac1cca504a512bc025b482e14f3fa1a
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-04-04 16:41:04 -0300

    Adicionando BD_ProjetoOficina

--- Aula 15/BD_ProjetoOficina.sql
+++ Aula 15/BD_ProjetoOficina.sql
@@ -5,21 +5,22 @@ USE ProjetoOficina;
 CREATE TABLE Cliente (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    sexo CHAR(1)
+    sexo CHAR(1) NOT NULL
 );
 
 CREATE TABLE TipoTelefone (
-    Id_TipoTelefone INT PRIMARY KEY,
-    TipoTelefone VARCHAR(20)
+    id_TipoTelefone INT PRIMARY KEY,
+    TipoTelefone VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Telefone (
     id_telefone INT AUTO_INCREMENT PRIMARY KEY,
-    fk_id_cliente INT NOT NULL,
     numero VARCHAR(20),
     DDD CHAR(2),
-    FOREIGN KEY (fk_id_cliente) REFERENCES Cliente(id_cliente),
-    FOREIGN KEY (fk_id_TipoTelefone) REFERENCES Cliente(id_cliente) ON DELETE CASCADE
+    fk_id_cliente INT,
+    fk_id_TipoTelefone INT,
+    FOREIGN KEY (fk_id_cliente) REFERENCES Cliente(id_cliente) ON DELETE CASCADE,
+    FOREIGN KEY (fk_id_TipoTelefone) REFERENCES TipoTelefone(id_TipoTelefone) ON DELETE CASCADE
 );
 
 CREATE TABLE Marca (
@@ -35,14 +36,15 @@ CREATE TABLE Modelo (
 );
 
 CREATE TABLE TipoCor (
-    id_TipocCor INT PRIMARY KEY,
+    id_TipoCor INT PRIMARY KEY,
     TipoCor VARCHAR(20)
 );
 
 CREATE TABLE Cor (
     id_cor INT AUTO_INCREMENT PRIMARY KEY,
     cor VARCHAR(20) NOT NULL,
-    fk_id_TipoCor INT NOT NULL
+    fk_id_TipoCor INT NOT NULL,
+    FOREIGN KEY (fk_id_TipoCor) REFERENCES TipoCor(id_TipoCor)
 );
 
 CREATE TABLE Carro (
@@ -50,6 +52,14 @@ CREATE TABLE Carro (
     placa VARCHAR(10),
     fk_id_cliente INT,
     fk_id_modelo INT,
-    FOREIGN KEY (fk_id_cliente) REFERENCES Cliente(id_cliente),
+    FOREIGN KEY (fk_id_cliente) REFERENCES Cliente(id_cliente) ON DELETE CASCADE,
     FOREIGN KEY (fk_id_modelo) REFERENCES Modelo(id_modelo) ON DELETE CASCADE
+);
+
+CREATE TABLE Carro_Cor (
+    fk_id_carro INT,
+    fk_id_cor INT,
+    PRIMARY KEY (fk_id_carro, fk_id_cor),
+    FOREIGN KEY (fk_id_carro) REFERENCES Carro(id_carro) ON DELETE CASCADE,
+    FOREIGN KEY (fk_id_cor) REFERENCES Cor(id_cor) ON DELETE CASCADE
 );
