c3ba66abb55c2c730c6470d9b94bf0f29ded26df
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-04-16 17:53:20 -0300

    Adicionando Inserts Funcionario_Departamento_110

---
+++ Aula 19/Inserts_Funcionario_Departamento_110.sql
@@ -0,0 +1,6 @@
+-- Inserts para a Tabela Departamento
+
+insert into Departamento (Id_departamento, Departamento, Admissao, Salario, Cargo) values
+(null, 'Financeiro', '2025-01-13', 5500.00, 'Analista Financeiro'),
+(null, 'Tecnologia da Informação', '2025-02-20', 8000.50, 'Analista de Sistemas'),
+(null, 'Recursos Humanos', '2024-12-01', 6200.75, 'Analista de Recursos Humanos');
