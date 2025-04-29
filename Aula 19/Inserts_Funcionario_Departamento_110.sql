b86d0eb23ba6e2254139b00f8971b0ccb4903790
leandro <leandrosousajesus@gmail.com>
2025-04-29 13:55:40 -0300

    Ajustando Inserts BD_Funcionario_Departamento

--- Aula 19/Inserts_Funcionario_Departamento_110.sql
+++ Aula 19/Inserts_Funcionario_Departamento_110.sql
@@ -5,19 +5,19 @@ insert into Departamento (Id_departamento, Departamento, Admissao, Salario, Carg
 (null, 'Tecnologia da Informação', '2025-02-20', 8000.50, 'Analista de Sistemas'),
 (null, 'Recursos Humanos', '2024-12-01', 6200.75, 'Analista de Recursos Humanos');
 
-insert into Funcionario (Id_funcionario, Nome, Id_departamento, Sexo, Email, fk_Departamento_Id_departamento) values
-(null, 'Ana Silva', 1, 'F', 'anasilva@gmail.com', 1),
-(null, 'Carlos Souza', 2, 'M', 'carlossouza@gmail.com', 2),
-(null, 'Maria Oliveira', 3, 'F', 'mariaoliveira@hotmail.com', 3),
-(null, 'João Santos', 1, 'M', 'joaosantos@icloud.com', 1),
-(null, 'Fernanda Lima', 2, 'F', 'fernandalima@gmail.com', 2),
-(null, 'Ricardo Almeida', 3, 'M', 'ricardoalmeida.hotmail.com', 3),
-(null, 'Patrícia Costa', 1, 'F', 'patriciacosta@gmail.com', 1),
-(null, 'Lucas Pereira', 2, 'M', 'lucaspereira@icloud.com', 2),
-(null, 'Juliana Martins', 3, 'F', 'julianamartins@gmail.com', 3),
-(null, 'Roberto Ferreira', 1, 'M', 'robertoferreira@hotmail.com', 1),
-(null, 'Tatiane Rocha', 2, 'F', 'tatianerocha@gmail.com', 2),
-(null, 'André Mendes', 3, 'M', 'andremendes@gmail.com', 3),
-(null, 'Camila Santos', 1, 'F', 'camilasantos@hotmail.com', 1),
-(null, 'Felipe Costa', 2, 'M', 'felipecosta@gmail.com', 2),
-(null, 'Larissa Almeida', 3, 'F','larissaalmeida@gmail.com', 3);
+insert into Funcionario (Id_funcionario, Nome, Sexo, Email, fk_Departamento_Id_departamento) values
+(null, 'Ana Silva', 'F', 'anasilva@gmail.com', 1),
+(null, 'Carlos Souza', 'M', 'carlossouza@gmail.com', 2),
+(null, 'Maria Oliveira', 'F', 'mariaoliveira@hotmail.com', 3),
+(null, 'João Santos', 'M', 'joaosantos@icloud.com', 1),
+(null, 'Fernanda Lima', 'F', 'fernandalima@gmail.com', 2),
+(null, 'Ricardo Almeida', 'M', 'ricardoalmeida.hotmail.com', 3),
+(null, 'Patrícia Costa', 'F', 'patriciacosta@gmail.com', 1),
+(null, 'Lucas Pereira', 'M', 'lucaspereira@icloud.com', 2),
+(null, 'Juliana Martins', 'F', 'julianamartins@gmail.com', 3),
+(null, 'Roberto Ferreira', 'M', 'robertoferreira@hotmail.com', 1),
+(null, 'Tatiane Rocha', 'F', 'tatianerocha@gmail.com', 2),
+(null, 'André Mendes', 'M', 'andremendes@gmail.com', 3),
+(null, 'Camila Santos', 'F', 'camilasantos@hotmail.com', 1),
+(null, 'Felipe Costa', 'M', 'felipecosta@gmail.com', 2),
+(null, 'Larissa Almeida', 'F','larissaalmeida@gmail.com', 3);
